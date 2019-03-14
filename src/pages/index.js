17238bb035df7387eaeed039e191e2724ec70f73
Ryan Workman <rdavid1099@gmail.com>
2019-03-13 17:30:24 -0700

    Fix spelling error 'technology'

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,12 +10,12 @@ const IndexPage = () => (
     <h1>Under Construction</h1>
     <h4>A new home for software developers is being built</h4>
     <p>
-      Want to help us buid a community focused on helping each other connect, have fun with technolyo, make friends, and so much more?<br />
+      Want to help us buid a community focused on helping each other connect, have fun with technology, make friends, and so much more?<br />
       Request an invite to our Slack workspace.
     </p>
 
     <SlackInviteForm />
-    
+
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }} />
   </Layout>
 )
