5994650f93a63b27c4139b1182c0a7a04923673b
Jasmin Hudacsek <jasmin@hudacsek.com>
2019-03-16 12:08:19 -0700

    Add styled-components library

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `LA Devs`,
-    description: `A new home for software developers is being built`,
+    description: `A new home for LA-based software developers is being built`,
     author: `Jasmin && Ryan`,
   },
   plugins: [
@@ -27,6 +27,7 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    `gatsby-plugin-styled-components`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // 'gatsby-plugin-offline',
