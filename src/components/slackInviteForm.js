96d46283ba81718fbc3ecadd2dfd9d177389acc6
Ryan Workman <rdavid1099@gmail.com>
2019-03-16 12:31:41 -0700

    Change form component to have state tracking email validation

--- src/components/slackInviteForm.js
+++ src/components/slackInviteForm.js
@@ -1,10 +1,23 @@
-import React from "react"
+import React from 'react'
 
-const SlackInviteForm = () => (
-  <form action="https://formspree.io/info@ladevs.org" method="POST">
-    <input type="email" name="_invte_requested" placeholder="Email Address" />
-    <input type="submit" value="Request Invite" />
-  </form>
-);
+import constants from '../utils/constants';
+
+class SlackInviteForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      validEmail: false,
+    };
+  }
+
+  render () {
+    return (
+      <form action={`https://formspree.io/${constants.LA_DEVS_EMAIL}`} method='POST'>
+        <input type='email' name='_invte_requested' placeholder='Email Address' />
+        <input type='submit' value='Request Invite' />
+      </form>
+    );
+  }
+}
 
 export default SlackInviteForm;
