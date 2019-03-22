13f9066b40a6d38185869988344798a5a99598ce
Ryan Workman <rdavid1099@gmail.com>
2019-03-22 12:55:48 -0700

    Fix seo title since auto appends with 'LA Devs'

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,4 @@
 export const EMAIL_REGEX = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 export const LA_DEVS_EMAIL = 'info@ladevs.org';
 export const SEO_TAGS = ['los angeles', 'la', 'slack', 'software', 'developers', 'networking', 'community'];
+export const SEO_TITLE = 'A Community for Los Angeles Based Software Developers';
