d7ef2f800570ff986863fdcfc5909d170cb6c369
Jasmin Hudacsek <jasmin@hudacsek.com>
2019-04-07 15:21:41 -0700

    Stylize content of layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,12 +5,18 @@
  * See: https://www.gatsbyjs.org/docs/static-query/
  */
 
-import React from "react"
-import PropTypes from "prop-types"
-import { StaticQuery, graphql } from "gatsby"
+import React from 'react';
+import PropTypes from 'prop-types';
+import { StaticQuery, graphql } from 'gatsby';
 
-import Header from "./header"
-import "./layout.css"
+import Header from './header';
+import styled from 'styled-components';
+
+const Content = styled.div`
+  margin: 0 auto;
+  max-width: 960px;
+  text-align: ${props => props.center ? 'center' : 'left'};
+`
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -26,21 +32,14 @@ const Layout = ({ children }) => (
     render={data => (
       <>
         <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
+        <Content center>
           <main>{children}</main>
-          <footer>
-            © {new Date().getFullYear()}, Built with ❤️ using
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </footer>
-        </div>
+        </Content>
+        <footer>
+          © {new Date().getFullYear()}, Built with ❤️ using
+          {` `}
+          <a href="https://www.gatsbyjs.org">Gatsby</a>
+        </footer>
       </>
     )}
   />
