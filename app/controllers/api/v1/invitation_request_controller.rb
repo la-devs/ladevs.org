812e39abada4399a92964ac487c0e9fdaaf95719
Ryan Workman <rdavid1099@gmail.com>
2019-07-13 11:53:19 -0700

    Wire form to function with internal API and send slack messages

--- app/controllers/api/v1/invitation_request_controller.rb
+++ app/controllers/api/v1/invitation_request_controller.rb
@@ -1,8 +1,13 @@
 class Api::V1::InvitationRequestController < Api::ApiController
   def create
+    binding.pry
     response = SlackMsgr.chat(:post_message, {
       channel: ENV['SLACK_INVITE_CHANNEL'],
+<<<<<<< HEAD
       text: "EMAIL REQUEST: #{params['_invite_requested']}"
+=======
+      text: 'testing :noodles:'
+>>>>>>> Wire form to function with internal API and send slack messages
     })
 
     redirect_to root_path
