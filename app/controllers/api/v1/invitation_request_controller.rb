50f6cfc39472ab089759dd4c4b73a0f9efd21bf5
Ryan Workman <rdavid1099@users.noreply.github.com>
2019-10-20 14:40:09 -0700

    Improve wiring of slack msgr to include messaging functionality

--- app/controllers/api/v1/invitation_request_controller.rb
+++ app/controllers/api/v1/invitation_request_controller.rb
@@ -1,15 +1,10 @@
 class Api::V1::InvitationRequestController < Api::ApiController
   def create
-    binding.pry
     response = SlackMsgr.chat(:post_message, {
       channel: ENV['SLACK_INVITE_CHANNEL'],
-<<<<<<< HEAD
-      text: "EMAIL REQUEST: #{params['_invite_requested']}"
-=======
-      text: 'testing :noodles:'
->>>>>>> Wire form to function with internal API and send slack messages
+      text: "EMAIL REQUEST: #{params['invite_requested']}"
     })
 
-    redirect_to root_path
+    render json: response
   end
 end
